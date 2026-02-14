18c8a2dedd082c48f8bc95bf0f45170745a92404
Heidi Schellman <33669005+hschellman@users.noreply.github.com>
2026-02-14 14:47:38 -0800

    make a tarball

---
+++ _includes/gitadd.sh
@@ -0,0 +1,13 @@
+git add *.sh *.py setup-grid DUNE*json
+tar cBf - \
+extractor_new.py \
+gitadd.sh \
+job_config.sh \
+makercds.sh \
+maketar.sh \
+setup_before_submit.sh \
+submit_local_code.jobscript.sh \
+setup-grid \
+submit_workflow.sh \
+> ../files/usefulcode.tar
+git add ../files/usefulcode.tar
